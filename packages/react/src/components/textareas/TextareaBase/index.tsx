10235b41f0b4172616eef5a4eb80b68362cb0a02
Domas <domas.bitvinskas@me.com>
2024-03-04 00:48:50 +0000

    Add textarea style

--- packages/react/src/components/textareas/TextareaBase/index.tsx
+++ packages/react/src/components/textareas/TextareaBase/index.tsx
@@ -7,22 +7,28 @@ type Props = React.ComponentProps<typeof TextareaAutosize>
 
 export const TextareaBase = forwardRef(function TextareaBase(props: Props, ref) {
   return (
-    <TextareaAutosize
-      // @ts-ignore-next-line
-      ref={ref}
-      className="textarea-base"
-      style={{
-        resize: 'none',
-        fontSize: `${14 * UPSCALE_RATIO}px`,
-        lineHeight: `${24 * UPSCALE_RATIO}px`,
-        transform: `scale(${1 / UPSCALE_RATIO})`,
-        margin: `0 ${(-100 * UPSCALE_RATIO + 100) / 2}%`,
-        width: `${100 * UPSCALE_RATIO}%`,
-        maxWidth: `${100 * UPSCALE_RATIO}%`,
-        flexGrow: 1,
-        display: 'flex',
-      }}
-      {...props}
-    />
+    <>
+      <style>
+        {`.superinterface-textarea { minHeight: inherit; height: 30px; }`}
+      </style>
+
+      <TextareaAutosize
+        // @ts-ignore-next-line
+        ref={ref}
+        className="superinterface-textarea"
+        style={{
+          resize: 'none',
+          fontSize: `${14 * UPSCALE_RATIO}px`,
+          lineHeight: `${24 * UPSCALE_RATIO}px`,
+          transform: `scale(${1 / UPSCALE_RATIO})`,
+          margin: `0 ${(-100 * UPSCALE_RATIO + 100) / 2}%`,
+          width: `${100 * UPSCALE_RATIO}%`,
+          maxWidth: `${100 * UPSCALE_RATIO}%`,
+          flexGrow: 1,
+          display: 'flex',
+        }}
+        {...props}
+      />
+    </>
   )
 })
