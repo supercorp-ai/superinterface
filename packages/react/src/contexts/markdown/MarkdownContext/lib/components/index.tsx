f766a2fda035d40a0d5fdef2eaf5cf0aefedd5f3
Domas <domas.bitvinskas@me.com>
2024-08-19 19:09:22 +0300

    Add Heading markdown support

--- packages/react/src/contexts/markdown/MarkdownContext/lib/components/index.tsx
+++ packages/react/src/contexts/markdown/MarkdownContext/lib/components/index.tsx
@@ -1,3 +1,4 @@
+import { Heading } from '@radix-ui/themes'
 import { Paragraph } from './Paragraph'
 import { Link } from './Link'
 import { UnorderedList } from './UnorderedList'
@@ -20,4 +21,10 @@ export const components = {
   code: Code,
   img: Img,
   annotation: Annotation,
+  h1: (props:  JSX.IntrinsicElements['h1']) => <Heading as="h1">{props.children}</Heading>,
+  h2: (props:  JSX.IntrinsicElements['h2']) => <Heading as="h2" size="5">{props.children}</Heading>,
+  h3: (props:  JSX.IntrinsicElements['h3']) => <Heading as="h3" size="4">{props.children}</Heading>,
+  h4: (props:  JSX.IntrinsicElements['h4']) => <Heading as="h4" size="3">{props.children}</Heading>,
+  h5: (props:  JSX.IntrinsicElements['h5']) => <Heading as="h5" size="3">{props.children}</Heading>,
+  h6: (props:  JSX.IntrinsicElements['h6']) => <Heading as="h6" size="3">{props.children}</Heading>,
 }
