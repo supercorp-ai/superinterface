f45e92057b88af5aaf4961d22294f516a9dbe964
Domas <domas.bitvinskas@me.com>
2024-10-04 21:57:12 +0300

    Move to threadRunRequiresAction

--- packages/react/src/hooks/messages/useCreateMessage/lib/mutationOptions/mutationFn/handleResponse/handlers/index.ts
+++ packages/react/src/hooks/messages/useCreateMessage/lib/mutationOptions/mutationFn/handleResponse/handlers/index.ts
@@ -6,6 +6,7 @@ import { threadRunCreated } from './threadRunCreated'
 import { threadRunStepCreated } from './threadRunStepCreated'
 import { threadRunStepDelta } from './threadRunStepDelta'
 import { threadRunStepCompleted } from './threadRunStepCompleted'
+import { threadRunRequiresAction } from './threadRunRequiresAction'
 
 export const handlers = {
   'thread.created': threadCreated,
@@ -16,4 +17,5 @@ export const handlers = {
   'thread.run.step.created': threadRunStepCreated,
   'thread.run.step.delta': threadRunStepDelta,
   'thread.run.step.completed': threadRunStepCompleted,
+  'thread.run.requires_action': threadRunRequiresAction,
 }
--- packages/react/src/hooks/messages/useCreateMessage/lib/mutationOptions/mutationFn/handleResponse/index.ts
+++ packages/react/src/hooks/messages/useCreateMessage/lib/mutationOptions/mutationFn/handleResponse/index.ts
@@ -1,9 +1,8 @@
 import OpenAI from 'openai'
-import { map } from 'radash'
 import {
   useQueryClient,
 } from '@tanstack/react-query'
-import { MessagesQueryKey, ToolCall } from '@/types'
+import { MessagesQueryKey } from '@/types'
 import { useSuperinterfaceContext } from '@/hooks/core/useSuperinterfaceContext'
 import { handlers } from './handlers'
 
@@ -20,60 +19,8 @@ export const handleResponse = ({
   queryClient: ReturnType<typeof useQueryClient>
   superinterfaceContext: ReturnType<typeof useSuperinterfaceContext>
 }) => {
-  const finalHandlers = {
-    ...handlers,
-    ...{
-      'thread.run.requires_action': async ({
-        value
-      }: any) => {
-        if (value.data.required_action.type === 'submit_client_tool_outputs') {
-          const toolCalls = value.data.required_action.submit_client_tool_outputs.tool_calls
-
-          const toolOutputs = await map(toolCalls, async (toolCall: ToolCall) => {
-            if (toolCall.type !== 'function') {
-              return {
-                toolCallId: toolCall.id,
-                output: `Error: client tool type ${toolCall.type} is not supported.`,
-              }
-            }
-
-            // @ts-ignore-next-line
-            const fn = window[toolCall.function.name as string]
-
-            if (!fn) {
-              return {
-                toolCallId: toolCall.id,
-                output: `Error: client function ${toolCall.function.name} is not defined.`,
-              }
-            }
-
-            const args = toolCall.function.arguments
-            const parsedArgs = JSON.parse(args)
-
-            const output = await fn(parsedArgs)
-
-            return {
-              toolCallId: toolCall.id,
-              output,
-            }
-          })
-
-          return fetch(`${superinterfaceContext.baseUrl}/threads/runs/submit-client-tool-outputs`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              toolOutputs,
-              ...superinterfaceContext.variables,
-            })
-          })
-        }
-      },
-    }
-  }
   // @ts-ignore-next-line
-  const handler = finalHandlers[value.value.event]
+  const handler = handlers[value.value.event]
 
   if (!handler) {
     return console.log('Missing handler', { value })
