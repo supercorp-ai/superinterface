fc36b6d376600f4f6c4b6ae5f9750d43a2d6afe4
Domas <domas.bitvinskas@me.com>
2024-10-13 21:40:16 +0300

    Improve annotation and link handling

---
+++ examples/next-frontend-functions/src/app/thread/page.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { useState } from 'react'
+import {
+  SuperinterfaceProvider,
+  Thread,
+  AssistantNameContext,
+} from '@superinterface/react'
+import { Theme } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+export default function Page() {
+  const [queryClient] = useState(() => (
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          // With SSR, we usually want to set some default staleTime
+          // above 0 to avoid refetching immediately on the client
+          staleTime: 10000,
+        },
+      },
+    })
+  ))
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Theme
+        accentColor="blue"
+        grayColor="gray"
+        appearance="light"
+        radius="medium"
+        scaling="100%"
+      >
+        <SuperinterfaceProvider
+          variables={{
+            publicApiKey: '37245be8-902a-440e-aaae-c56151fe8acc',
+            assistantId: '13b80702-9d30-44db-84b6-85764921f00c',
+          }}
+        >
+          <AssistantNameContext.Provider value="Annotations tester">
+            <Thread />
+          </AssistantNameContext.Provider>
+        </SuperinterfaceProvider>
+      </Theme>
+    </QueryClientProvider>
+  )
+}
--- packages/react/src/lib/remark/remarkAnnotation.ts
+++ packages/react/src/lib/remark/remarkAnnotation.ts
@@ -1,20 +1,28 @@
 import OpenAI from 'openai'
 import { isNumber } from 'radash'
-import type { Literal } from 'unist'
+import type { Node, Literal, Position } from 'unist'
+import type { Text, Link } from 'mdast'
 // @ts-ignore-next-line
 import flatMap from 'unist-util-flatmap'
 
-type Node = Literal & {
+interface AnnotationNode extends Literal {
+  type: 'annotation'
   value: string
+  position?: Position
+  data: {
+    hName: 'annotation'
+    hProperties: {
+      annotation: any
+    }
+  }
 }
 
 const sortedAnnotations = ({
   content,
 }: {
   content: OpenAI.Beta.Threads.Messages.TextContentBlock
-}) => (
+}) =>
   content.text.annotations.sort((a, b) => a.start_index - b.start_index)
-)
 
 export const remarkAnnotation = ({
   content,
@@ -24,133 +32,170 @@ export const remarkAnnotation = ({
   return () => {
     return (tree: any) => {
       flatMap(tree, (node: Node) => {
-        if (!['text', 'link'].includes(node.type)) {
+        // Process text and link nodes
+        if (node.type === 'text' || node.type === 'link') {
+          return processNodeWithAnnotations({ node, content })
+        } else {
+          // Return other nodes as-is
           return [node]
         }
-
-        if (!content.text?.annotations?.length) {
-          return [node]
-        }
-
-        if (!node.position) {
-          return [node]
-        }
-
-        const nodeStart = node.position.start.offset
-
-        if (!isNumber(nodeStart)) {
-          return [node]
-        }
-
-        const nodeEnd = node.position.end.offset
-
-        if (!isNumber(nodeEnd)) {
-          return [node]
-        }
-
-        const newNodes: Node[] = []
-
-        let lastProcessedIndex = nodeStart
-
-        sortedAnnotations({ content }).forEach((annotation) => {
-          const annotationStart = annotation.start_index
-          const annotationEnd = annotation.end_index
-
-          if (nodeEnd < annotationStart || nodeStart > annotationEnd) {
-            return
-          }
-
-          if (node.type === 'link') {
-            if (annotation.type === 'file_path') {
-              console.log({ annotation, node })
-              newNodes.push({
-                ...node,
-                type: 'annotation',
-                // @ts-ignore-next-line
-                url: annotation.file_path.file_id,
-                data: {
-                  hName: 'annotation',
-                  hProperties: {
-                    annotation,
-                  },
-                },
-              })
-            }
-
-            return
-          }
-
-          const startIndex = Math.max(nodeStart, annotationStart)
-          const endIndex = Math.min(nodeEnd, annotationEnd)
-
-          if (lastProcessedIndex < startIndex) {
-            newNodes.push({
-              type: 'text',
-              value: node.value.slice(lastProcessedIndex - nodeStart, startIndex - nodeStart),
-              position: {
-                start: {
-                  line: node.position!.start.line,
-                  column: node.position!.start.column,
-                  offset: lastProcessedIndex,
-                },
-                end: {
-                  line: node.position!.end.line,
-                  column: node.position!.end.column,
-                  offset: startIndex,
-                },
-              },
-            })
-          }
-
-          newNodes.push({
-            type: 'annotation',
-            value: node.value.slice(startIndex - nodeStart, endIndex - nodeStart),
-            position: {
-              start: {
-                line: node.position!.start.line,
-                column: node.position!.start.column,
-                offset: startIndex,
-              },
-              end: {
-                line: node.position!.end.line,
-                column: node.position!.end.column,
-                offset: endIndex,
-              },
-            },
-            data: {
-              hName: 'annotation',
-              hProperties: {
-                annotation,
-              },
-            },
-          })
-
-          lastProcessedIndex = endIndex
-        })
-
-        if (node.type === 'text') {
-          if (lastProcessedIndex < nodeEnd) {
-            newNodes.push({
-              type: 'text',
-              value: node.value.slice(lastProcessedIndex - nodeStart, nodeEnd - nodeStart),
-              position: {
-                start: {
-                  line: node.position!.start.line,
-                  column: node.position!.start.column,
-                  offset: lastProcessedIndex,
-                },
-                end: {
-                  line: node.position!.end.line,
-                  column: node.position!.end.column,
-                  offset: nodeEnd,
-                },
-              },
-            })
-          }
-        }
-
-        return newNodes
       })
     }
+  }
+}
+
+const processNodeWithAnnotations = ({
+  node,
+  content,
+}: {
+  node: Node
+  content: OpenAI.Beta.Threads.Messages.TextContentBlock
+}): Node[] => {
+  if (!content.text?.annotations?.length) {
+    return [node]
+  }
+
+  if (!node.position) {
+    return [node]
+  }
+
+  const annotations = sortedAnnotations({ content })
+
+  if (node.type === 'text') {
+    // node is a Text node
+    return processTextNode({ node: node as Text, annotations })
+  } else if (node.type === 'link') {
+    // node is a Link node
+    const linkNode = node as Link
+    // Process link node's children
+    linkNode.children = flatMap(linkNode.children, (childNode: Node) => {
+      if (childNode.type === 'text') {
+        return processTextNode({ node: childNode as Text, annotations })
+      } else {
+        return [childNode]
+      }
+    })
+    return [linkNode]
+  } else {
+    // Return node as-is
+    return [node]
+  }
+}
+
+const processTextNode = ({
+  node,
+  annotations,
+}: {
+  node: Text
+  annotations: any[]
+}): Node[] => {
+  if (!node.position || !node.value) {
+    return [node]
+  }
+
+  const nodeStart = node.position.start.offset!
+  const nodeEnd = node.position.end.offset!
+
+  if (!isNumber(nodeStart) || !isNumber(nodeEnd)) {
+    return [node]
+  }
+
+  const newNodes: Node[] = []
+  let lastIndex = nodeStart
+
+  annotations.forEach((annotation) => {
+    const annotationStart = annotation.start_index
+    const annotationEnd = annotation.end_index
+
+    if (nodeEnd <= annotationStart || nodeStart >= annotationEnd) {
+      return
+    }
+
+    const start = Math.max(nodeStart, annotationStart)
+    const end = Math.min(nodeEnd, annotationEnd)
+
+    if (lastIndex < start) {
+      newNodes.push(createTextNode({ node, startOffset: lastIndex, endOffset: start }))
+    }
+
+    newNodes.push(
+      createAnnotationNode({
+        node,
+        startOffset: start,
+        endOffset: end,
+        annotation,
+      })
+    )
+    lastIndex = end
+  })
+
+  if (lastIndex < nodeEnd) {
+    newNodes.push(createTextNode({ node, startOffset: lastIndex, endOffset: nodeEnd }))
+  }
+
+  return newNodes
+}
+
+const createTextNode = ({
+  node,
+  startOffset,
+  endOffset,
+}: {
+  node: Text
+  startOffset: number
+  endOffset: number
+}): Text => {
+  const valueStart = startOffset - node.position!.start.offset!
+  const valueEnd = endOffset - node.position!.start.offset!
+
+  return {
+    type: 'text',
+    value: node.value.slice(valueStart, valueEnd),
+    position: {
+      start: {
+        ...node.position!.start,
+        offset: startOffset,
+      },
+      end: {
+        ...node.position!.end,
+        offset: endOffset,
+      },
+    },
+  }
+}
+
+const createAnnotationNode = ({
+  node,
+  startOffset,
+  endOffset,
+  annotation,
+}: {
+  node: Text
+  startOffset: number
+  endOffset: number
+  annotation: any
+}): AnnotationNode => {
+  const valueStart = startOffset - node.position!.start.offset!
+  const valueEnd = endOffset - node.position!.start.offset!
+
+  return {
+    type: 'annotation',
+    value: node.value.slice(valueStart, valueEnd),
+    position: {
+      start: {
+        ...node.position!.start,
+        offset: startOffset,
+      },
+      end: {
+        ...node.position!.end,
+        offset: endOffset,
+      },
+    },
+    data: {
+      hName: 'annotation',
+      hProperties: {
+        annotation,
+      },
+    },
   }
 }
