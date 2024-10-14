114b74669a1f4e0ab185e4597ec061e71bec6b2e
Domas <domas.bitvinskas@me.com>
2024-10-14 23:12:48 +0300

    Add vite

---
+++ examples/vite-react-with-videos/src/AudioThread.jsx
@@ -0,0 +1,56 @@
+import ReactDOM from 'react-dom/client'
+import { useState } from 'react'
+import {
+  SuperinterfaceProvider,
+  Thread,
+  AssistantNameContext,
+} from '@superinterface/react'
+import { Theme } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const App = () => {
+  const [queryClient] = useState(() => (
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          // With SSR, we usually want to set some default staleTime
+          // above 0 to avoid refetching immediately on the client
+          staleTime: 10000,
+        },
+      },
+    })
+  ))
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Theme
+        accentColor="blue"
+        grayColor="gray"
+        appearance="light"
+        radius="medium"
+        scaling="100%"
+      >
+        <SuperinterfaceProvider
+          variables={{
+            publicApiKey: '37245be8-902a-440e-aaae-c56151fe8acc',
+            assistantId: '13b80702-9d30-44db-84b6-85764921f00c',
+          }}
+        >
+          <AssistantNameContext.Provider value="Annotations tester">
+            <Thread />
+          </AssistantNameContext.Provider>
+        </SuperinterfaceProvider>
+      </Theme>
+    </QueryClientProvider>
+  )
+}
+
+const rootDiv = document.createElement('div');
+rootDiv.id = 'thread-root';
+document.body.appendChild(rootDiv);
+
+ReactDOM.createRoot(rootDiv).render(<App />)
