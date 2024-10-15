f4548ea534fab44b6a4044603f41cd7ffc487feb
Domas <domas.bitvinskas@me.com>
2024-10-15 03:24:58 +0300

    More progress

---
+++ examples/tsup-react/src/Thread.tsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+import {
+  Thread,
+} from '@superinterface/react'
+import { Providers } from '@/components/Providers'
+
+const element = document.createElement('div')
+element.classList.add('superinterface')
+document.body.appendChild(element)
+
+const root = createRoot(element)
+
+root.render(
+  <Providers>
+    <Thread />
+  </Providers>
+)
---
+++ examples/tsup-react/tsup.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
+  entry: [
+    'src/Thread.tsx',
+    'src/AudioThread.tsx',
+    'src/ThreadDialog.tsx',
+    'src/AudioThreadDialog.tsx',
+  ],
+  sourcemap: true,
+  clean: true,
+  shims: true,
+  platform: 'browser',
+  minify: true,
+  format: [
+    'iife',
+    'cjs',
+    'esm'
+  ],
+  injectStyle: true,
+  dts: false,
+  noExternal: [
+    /(.*)/
+  ],
+})
