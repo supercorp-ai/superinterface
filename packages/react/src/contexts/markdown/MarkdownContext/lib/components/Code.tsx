24659dc726509901edc2280d657df4f8fe1b11fe
Domas <domas.bitvinskas@me.com>
2024-11-14 20:54:49 +0200

    @superinterface/react@2.25.3; @superinterface/javascript@2.8.2: add Suggestions support by default

--- packages/react/src/contexts/markdown/MarkdownContext/lib/components/Code.tsx
+++ packages/react/src/contexts/markdown/MarkdownContext/lib/components/Code.tsx
@@ -1,9 +1,19 @@
 import {
   Code as RadixCode,
 } from '@radix-ui/themes'
+import { Suggestions } from '@/components/suggestions/Suggestions'
 
-export const Code = ({ children }: JSX.IntrinsicElements['code']) => (
-  <RadixCode>
-    {children}
-  </RadixCode>
-)
+export const Code = ({
+  children,
+  className,
+}: JSX.IntrinsicElements['code']) => {
+  if (className === 'language-suggestions') {
+    return <Suggestions>{children}</Suggestions>
+  }
+
+  return (
+    <RadixCode>
+      {children}
+    </RadixCode>
+  )
+}
