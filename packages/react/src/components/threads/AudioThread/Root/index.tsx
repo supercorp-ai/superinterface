a0b9b002df5afebcd87ae6247d8bd0855de92aec
Domas <domas.bitvinskas@me.com>
2025-01-01 22:18:26 +0200

    Better audioRuntime loading

--- packages/react/src/components/threads/AudioThread/Root/index.tsx
+++ packages/react/src/components/threads/AudioThread/Root/index.tsx
@@ -3,6 +3,7 @@
 import { Flex } from '@radix-ui/themes'
 import _ from 'lodash'
 import { AudioThreadContext } from '@/contexts/threads/AudioThreadContext'
+import { useAudioThreadContext } from '@/hooks/threads/useAudioThreadContext'
 import { ToastsProvider } from '@/components/toasts/ToastsProvider'
 import type { StyleProps, PlayArgs, AudioRuntime } from '@/types'
 import { useTtsAudioRuntime } from '@/hooks/audioRuntimes/useTtsAudioRuntime'
@@ -17,9 +18,25 @@ const Content = ({
   children,
   className,
   style,
+}: Args) => (
+  <Flex
+    direction="column"
+    flexGrow="1"
+    p="9"
+    className={className}
+    style={style}
+  >
+    {children}
+  </Flex>
+)
+
+const TtsAudioRuntime = ({
+  children,
   play,
-  audioRuntime,
-}: Args) => {
+}: {
+  children: React.ReactNode
+  play?: (args: PlayArgs) => void
+}) => {
   const { ttsAudioRuntime } = useTtsAudioRuntime({
     play,
   })
@@ -27,29 +44,73 @@ const Content = ({
   return (
     <AudioThreadContext.Provider
       value={{
-        audioRuntime: audioRuntime ?? ttsAudioRuntime,
+        audioRuntime: ttsAudioRuntime,
       }}
     >
-      <Flex
-        direction="column"
-        flexGrow="1"
-        p="9"
-        className={className}
-        style={style}
-      >
-        {children}
-      </Flex>
+      {children}
+    </AudioThreadContext.Provider>
+  )
+}
+
+const AudioRuntimeProvider = ({
+  children,
+  play,
+}: {
+  children: React.ReactNode
+  play?: (args: PlayArgs) => void
+}) => {
+  const audioThreadContext = useAudioThreadContext()
+
+  if (audioThreadContext.audioRuntime) {
+    return children
+  }
+
+  return (
+    <TtsAudioRuntime play={play}>
+      {children}
+    </TtsAudioRuntime>
+  )
+}
+
+const Provider = ({
+  children,
+  ...rest
+}: {
+  children: React.ReactNode
+}) => {
+  const audioThreadContext = useAudioThreadContext()
+
+  return (
+    <AudioThreadContext.Provider
+      value={{
+        ...audioThreadContext,
+        ...rest,
+      }}
+    >
+      {children}
     </AudioThreadContext.Provider>
   )
 }
 
 export const Root = ({
   children,
+  play,
+  className,
+  style,
   ...rest
 }: Args) => (
-  <ToastsProvider>
-    <Content {...rest}>
-      {children}
-    </Content>
-  </ToastsProvider>
+  <Provider {...rest}>
+    <AudioRuntimeProvider
+      play={play}
+    >
+      <ToastsProvider>
+        <Content
+          className={className}
+          style={style}
+        >
+          {children}
+        </Content>
+      </ToastsProvider>
+    </AudioRuntimeProvider>
+  </Provider>
 )
--- packages/react/src/hooks/audioThreads/useStatus/index.ts
+++ packages/react/src/hooks/audioThreads/useStatus/index.ts
@@ -1,26 +1,21 @@
 import { useMemo } from 'react'
 import { useAudioThreadContext } from '@/hooks/threads/useAudioThreadContext'
-import { useLatestMessage } from '@/hooks/messages/useLatestMessage'
 import { statusMessages } from './lib/statusMessages'
 
 export const useStatus = () => {
   const { audioRuntime } = useAudioThreadContext()
-  const latestMessageProps = useLatestMessage()
 
   const status = useMemo((): keyof typeof statusMessages => {
     if (audioRuntime.user.rawStatus === 'recording') return 'recording'
     if (audioRuntime.user.isPending) return 'creatingMessage'
-
     if (audioRuntime.assistant.paused || !audioRuntime.assistant.isAudioPlayed) return 'playerPaused'
     if (audioRuntime.assistant.playing || audioRuntime.assistant.isPending) return 'playing'
     if (!audioRuntime.assistant.isAudioPlayed && !audioRuntime.assistant.isReady) return 'loading'
-    if (latestMessageProps.latestMessage?.status === 'in_progress') return 'creatingMessage'
     if (audioRuntime.user.rawStatus === 'idle') return 'idle'
     if (audioRuntime.user.rawStatus === 'paused') return 'recorderPaused'
 
     return 'loading'
   }, [
-    latestMessageProps,
     audioRuntime,
   ])
 
