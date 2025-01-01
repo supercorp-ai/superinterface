a5d26d8115b94f28c320b6fbe4cfc88037effdf0
Domas <domas.bitvinskas@me.com>
2025-01-02 00:48:27 +0200

    @superinterface/javascript@3.3.0: add support for webrtc realtime api

---
+++ packages/javascript/src/AudioThreadDialogWebrtcAudioRuntime.tsx
@@ -0,0 +1,20 @@
+import { createRoot } from 'react-dom/client'
+import {
+  AudioThreadDialog,
+  WebrtcAudioRuntimeProvider,
+} from '@superinterface/react'
+import { rootElement } from '@superinterface/root-element'
+import { Providers } from '@/components/Providers'
+import { superinterfaceContext as getSuperinterfaceContext } from '@/lib/superinterfaceContext'
+
+const currentScript = document.currentScript
+const superinterfaceContext = getSuperinterfaceContext({ currentScript })
+const root = createRoot(rootElement({ currentScript }))
+
+root.render(
+  <Providers superinterfaceContext={superinterfaceContext}>
+    <WebrtcAudioRuntimeProvider>
+      <AudioThreadDialog />
+    </WebrtcAudioRuntimeProvider>
+  </Providers>
+)
