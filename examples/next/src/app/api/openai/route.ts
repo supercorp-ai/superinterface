fb092fb13067fc7bdf0c1e474a584a23f94f5dc4
Domas <domas.bitvinskas@me.com>
2025-01-07 01:51:22 +0200

    Add OpenAI server route

---
+++ examples/next/src/app/api/openai/route.ts
@@ -0,0 +1,21 @@
+import { type NextRequest, NextResponse } from 'next/server'
+import OpenAI from 'openai'
+import {
+  messagesResponse,
+} from '@superinterface/react/server'
+
+export const GET = async () => {
+  const client = new OpenAI({
+    apiKey: process.env.RENAMED_OPENAI_API_KEY,
+  })
+
+  const pageParam = null
+
+  const r = await messagesResponse({
+    threadId: 'thread_BWZSFgxf4o7x1psewUjwhitt',
+    client,
+    ...(pageParam ? { pageParam } : {}),
+  })
+
+  return NextResponse.json(r)
+}
