c4fa0e480c3d914178a6462662b67e12f2231a41
Domas <domas.bitvinskas@me.com>
2025-01-07 02:18:27 +0200

    Error nicely

--- examples/next/src/app/api/openai/route.ts
+++ examples/next/src/app/api/openai/route.ts
@@ -5,17 +5,23 @@ import {
 } from '@superinterface/react/server'
 
 export const GET = async () => {
-  const client = new OpenAI({
-    apiKey: 'sk-proj-11111111111111111111',
-  })
+  try {
+    const client = new OpenAI({
+      apiKey: 'sk-proj-11111111111111111111',
+    })
 
-  const pageParam = null
+    const pageParam = null
 
-  const r = await messagesResponse({
-    threadId: 'thread_BWZSFgxf4o7x1psewUjwhitt',
-    client,
-    ...(pageParam ? { pageParam } : {}),
-  })
+    const r = await messagesResponse({
+      threadId: 'thread_BWZSFgxf4o7x1psewUjwhitt',
+      client,
+      ...(pageParam ? { pageParam } : {}),
+    })
 
-  return NextResponse.json(r)
+    return NextResponse.json(r)
+  } catch (e) {
+    return NextResponse.json({
+      error: 'Provide a valid API key',
+    })
+  }
 }
