c494ab6162f037178df3c268e0241af4d878cd3b
Domas <domas.bitvinskas@me.com>
2025-01-28 18:12:01 +0200

    @superinterface/react@3.16.3: don’t pass twillio udp param

--- packages/react/src/hooks/audioRuntimes/useWebrtcAudioRuntime/index.ts
+++ packages/react/src/hooks/audioRuntimes/useWebrtcAudioRuntime/index.ts
@@ -130,7 +130,7 @@ export const useWebrtcAudioRuntime = () => {
           { urls: 'stun:stun2.l.google.com:19302' },
           { urls: 'stun:stun3.l.google.com:19302' },
           { urls: 'stun:stun4.l.google.com:19302' },
-          { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
+          { urls: 'stun:global.stun.twilio.com:3478' },
         ],
       })
 
