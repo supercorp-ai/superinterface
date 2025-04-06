624f69b0c4152fce0f068d5f601b16c51bbcfa1f
Domas Bitvinskas <domas.bitvinskas@me.com>
2025-04-06 20:30:14 +0300

    @superinterface/react@3.21.0: more AudioThread primitives

---
+++ examples/next-frontend-functions/src/app/audio-thread/primitives/page.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import { useState } from 'react'
+import {
+  SuperinterfaceProvider,
+  AudioThread,
+  AssistantNameContext,
+  useWebrtcAudioRuntime,
+} from '@superinterface/react'
+import { Theme } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const Content = () => {
+  const { webrtcAudioRuntime } = useWebrtcAudioRuntime()
+
+  return (
+    <AudioThread.Root
+      audioRuntime={webrtcAudioRuntime}
+    >
+      <AudioThread.Visualization.Root>
+        <AudioThread.Visualization.AssistantVisualization.Root
+          height="100px"
+          width="100px"
+        >
+          <AudioThread.Visualization.AssistantVisualization.BarsVisualizer
+            height="20px"
+            barWidth="10px"
+          />
+        </AudioThread.Visualization.AssistantVisualization.Root>
+      </AudioThread.Visualization.Root>
+      <AudioThread.Status />
+      <AudioThread.Form />
+    </AudioThread.Root>
+  )
+}
+
+export default function Page() {
+  const [queryClient] = useState(() => (
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          // With SSR, we usually want to set some default staleTime
+          // above 0 to avoid refetching immediately on the client
+          staleTime: 10000,
+        },
+      },
+    })
+  ))
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Theme
+        accentColor="blue"
+        grayColor="gray"
+        appearance="light"
+        radius="medium"
+        scaling="100%"
+      >
+        <SuperinterfaceProvider
+          // baseUrl="http://localhost:3000/api/cloud"
+          variables={{
+            publicApiKey: '37245be8-902a-440e-aaae-c56151fe8acc',
+            assistantId: '1e5e718b-d94c-4362-b4a5-8401583b958a',
+          }}
+        >
+          <AssistantNameContext.Provider value="Realtime tester">
+            <Content />
+          </AssistantNameContext.Provider>
+        </SuperinterfaceProvider>
+      </Theme>
+    </QueryClientProvider>
+  )
+}
--- packages/react/src/components/threads/AudioThread/Visualization/index.tsx
+++ packages/react/src/components/threads/AudioThread/Visualization/index.tsx
@@ -1,18 +1,26 @@
-import { useContext, useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useContext, createContext } from 'react'
 import _ from 'lodash'
 import { Flex } from '@radix-ui/themes'
 import { AssistantAvatar } from '@/components/messageGroups/MessageGroupBase/AssistantAvatar'
 import { Name } from '@/components/messageGroups/MessageGroupBase/Name'
 import { AssistantNameContext } from '@/contexts/assistants/AssistantNameContext'
 import { useAudioThreadContext } from '@/hooks/threads/useAudioThreadContext'
-import { BarsVisualizer } from '@/components/threads/AudioThread/BarsVisualizer'
+import { BarsVisualizer as AudioThreadBarsVisualizer } from '@/components/threads/AudioThread/BarsVisualizer'
 import { useStatus } from '@/hooks/audioThreads/useStatus'
 import type { StyleProps } from '@/types'
 
-export const Visualization = (props: StyleProps) => {
-  const { status } = useStatus()
+const AudioThreadVisualizationContext = createContext<{
+  scale: number
+}>({
+  scale: 0,
+})
+
+const Provider = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
   const audioThreadContext = useAudioThreadContext()
-  const assistantNameContext = useContext(AssistantNameContext)
   const [scale, setScale] = useState(0)
 
   const draw = useCallback(({ visualizationAnalyser }: { visualizationAnalyser: AnalyserNode | null }) => {
@@ -30,47 +38,130 @@ export const Visualization = (props: StyleProps) => {
   }, [])
 
   useEffect(() => {
-    draw({ visualizationAnalyser: audioThreadContext.audioRuntime.user.visualizationAnalyser })
+    draw({ visualizationAnalyser: audioThreadContext.audioRuntime!.user.visualizationAnalyser })
   }, [draw, audioThreadContext])
 
   return (
+    <AudioThreadVisualizationContext.Provider value={{ scale }}>
+      {children}
+    </AudioThreadVisualizationContext.Provider>
+  )
+}
+
+const Root = ({
+  children,
+  ...rest
+}: {
+  children: React.ReactNode
+}) => (
+  <Provider>
     <Flex
       direction="column"
       align="center"
       justify="center"
       mb="3"
       flexGrow="1"
-      {...props}
+      {...rest}
     >
-      <Flex
-        align="center"
-        justify="center"
-        height="200px"
-        width="200px"
-        style={{
-          backgroundColor: status === 'playing' ? 'var(--accent-4)' : 'var(--gray-4)',
-          borderRadius: '9999px',
-          scale,
-        }}
-      >
-        <BarsVisualizer
-          visualizationAnalyser={audioThreadContext.audioRuntime.assistant.visualizationAnalyser}
-          backgroundColor={status === 'playing' ? 'var(--accent-11)' : 'var(--gray-11)'}
-          height="40px"
-          barWidth="24px"
-        />
-      </Flex>
+      {children}
+    </Flex>
+  </Provider>
+)
 
-      <Flex
-        ml="-22.5px"
-        gap="3"
-        pt="5"
-      >
-        <AssistantAvatar />
-        <Name>
-          {assistantNameContext}
-        </Name>
-      </Flex>
+const BarsVisualizer = ({
+  height = '40px',
+  barWidth = '24px',
+  ...rest
+}: StyleProps & {
+  height?: string
+  barWidth?: string
+}) => {
+  const { status } = useStatus()
+  const audioThreadContext = useAudioThreadContext()
+
+  return (
+    <AudioThreadBarsVisualizer
+      visualizationAnalyser={audioThreadContext.audioRuntime!.assistant.visualizationAnalyser}
+      backgroundColor={status === 'playing' ? 'var(--accent-11)' : 'var(--gray-11)'}
+      height={height}
+      barWidth={barWidth}
+      {...rest}
+    />
+  )
+}
+
+const AssistantVisualizationRoot = ({
+  children,
+  height = '200px',
+  width = '200px',
+  ...rest
+}: StyleProps & {
+  children: React.ReactNode
+  height?: string
+  width?: string
+}) => {
+  const { status } = useStatus()
+  const { scale } = useContext(AudioThreadVisualizationContext)
+
+  return (
+    <Flex
+      align="center"
+      justify="center"
+      height={height}
+      width={width}
+      style={{
+        backgroundColor: status === 'playing' ? 'var(--accent-4)' : 'var(--gray-4)',
+        borderRadius: '9999px',
+        scale,
+        ...rest.style ?? {},
+      }}
+      {...rest}
+    >
+      {children}
     </Flex>
   )
 }
+
+const AssistantVisualization = (props: StyleProps & {
+  height?: string
+  width?: string
+}) => (
+  <AssistantVisualizationRoot
+    {...props}
+  >
+    <BarsVisualizer />
+  </AssistantVisualizationRoot>
+)
+
+AssistantVisualization.Root = AssistantVisualizationRoot
+AssistantVisualization.BarsVisualizer = BarsVisualizer
+
+const AssistantInfo = (props: StyleProps) => {
+  const assistantNameContext = useContext(AssistantNameContext)
+
+  return (
+    <Flex
+      ml="-22.5px"
+      gap="3"
+      pt="5"
+      {...props}
+    >
+      <AssistantAvatar />
+      <Name>
+        {assistantNameContext}
+      </Name>
+    </Flex>
+  )
+}
+
+export const Visualization = (props: StyleProps) => (
+  <Root {...props}>
+    <AssistantVisualization />
+    <AssistantInfo />
+  </Root>
+)
+
+Visualization.Root = Root
+Visualization.Provider = Provider
+Visualization.AssistantVisualization = AssistantVisualization
+Visualization.AssistantInfo = AssistantInfo
