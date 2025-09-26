75f9c5b0f8fe1b22d4521251128a646041a82067
Domas Bitvinskas <domas.bitvinskas@me.com>
2025-09-26 06:34:27 +0100

    Fix types

--- packages/react/src/components/textareas/TextareaBase/index.tsx
+++ packages/react/src/components/textareas/TextareaBase/index.tsx
@@ -4,34 +4,32 @@ import type { StyleProps } from '@/types'
 
 type Props = React.ComponentProps<typeof TextareaAutosize> & StyleProps
 
-export const TextareaBase = forwardRef(function TextareaBase({
-  style,
-  className,
-  ...rest
-}: Props, ref) {
-  return (
-    <>
-      <style>
-        {`.superinterface-textarea { min-height: inherit; height: 30px; }
+export const TextareaBase = forwardRef<HTMLTextAreaElement, Props>(
+  function TextareaBase({ style, className, ...rest }: Props, ref) {
+    return (
+      <>
+        <style>
+          {`.superinterface-textarea { min-height: inherit; height: 30px; }
 .superinterface-textarea::placeholder { color: var(--gray-a10); }`}
-      </style>
+        </style>
 
-      <TextareaAutosize
-        // @ts-ignore-next-line
-        ref={ref}
-        className={`rt-reset superinterface-textarea ${className ?? ''}`}
-        style={{
-          border: 0,
-          outline: 0,
-          boxSizing: 'border-box',
-          resize: 'none',
-          color: 'var(--gray-12)',
-          flexGrow: 1,
-          display: 'flex',
-          ...(style ?? {}),
-        }}
-        {...rest}
-      />
-    </>
-  )
-})
+        <TextareaAutosize
+          // @ts-ignore-next-line
+          ref={ref}
+          className={`rt-reset superinterface-textarea ${className ?? ''}`}
+          style={{
+            border: 0,
+            outline: 0,
+            boxSizing: 'border-box',
+            resize: 'none',
+            color: 'var(--gray-12)',
+            flexGrow: 1,
+            display: 'flex',
+            ...(style ?? {}),
+          }}
+          {...rest}
+        />
+      </>
+    )
+  },
+)
