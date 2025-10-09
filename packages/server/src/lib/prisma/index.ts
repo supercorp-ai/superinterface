d653bdbb0d92dae3620f2fdfbf26ef30485f9405
Domas Bitvinskas <domas.bitvinskas@me.com>
2025-10-09 03:03:11 +0300

    @superinterface/server@1.0.21: use prisma proxy

--- packages/server/src/lib/prisma/index.ts
+++ packages/server/src/lib/prisma/index.ts
@@ -1,6 +1,13 @@
 import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
 
+const prismaGlobalKey = Symbol.for('superinterface.server.prisma')
+
+type GlobalPrismaState = typeof globalThis & {
+  [prismaGlobalKey]?: PrismaClient
+  prisma?: PrismaClient
+}
+
 const prismaClientSingleton = () => {
   const connectionString = `${process.env.DATABASE_URL}`
 
@@ -20,29 +27,52 @@ const prismaClientSingleton = () => {
   })
 }
 
-const globalKey = '__superinterfaceServerPrisma'
-
 declare global {
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-  var __superinterfaceServerPrisma: undefined | PrismaClient
 }
 
 export const getPrisma = (): PrismaClient => {
-  const globalForPrisma = globalThis as typeof globalThis & {
-    [globalKey]?: PrismaClient
-    prisma?: PrismaClient
-  }
+  const globalForPrisma = globalThis as GlobalPrismaState
 
-  if (globalForPrisma[globalKey]) return globalForPrisma[globalKey]
+  if (globalForPrisma[prismaGlobalKey])
+    return globalForPrisma[prismaGlobalKey] as PrismaClient
   if (globalForPrisma.prisma) {
-    globalForPrisma[globalKey] = globalForPrisma.prisma
+    globalForPrisma[prismaGlobalKey] = globalForPrisma.prisma
     return globalForPrisma.prisma
   }
 
   const client = prismaClientSingleton()
-  globalForPrisma[globalKey] = client
+  globalForPrisma[prismaGlobalKey] = client
   if (!globalForPrisma.prisma) globalForPrisma.prisma = client
   return client
 }
 
-export const prisma = getPrisma()
+const getClient = (): PrismaClient => {
+  return getPrisma()
+}
+
+let prismaInstance: PrismaClient | undefined
+
+export const prisma = new Proxy({} as PrismaClient, {
+  get(_target, prop, receiver) {
+    const client = prismaInstance ?? (prismaInstance = getClient())
+    const value = Reflect.get(client, prop, receiver)
+    return typeof value === 'function' ? value.bind(client) : value
+  },
+  set(_target, prop, value, receiver) {
+    const client = prismaInstance ?? (prismaInstance = getClient())
+    return Reflect.set(client, prop, value, receiver)
+  },
+  has(_target, prop) {
+    const client = prismaInstance ?? (prismaInstance = getClient())
+    return Reflect.has(client, prop)
+  },
+  ownKeys() {
+    const client = prismaInstance ?? (prismaInstance = getClient())
+    return Reflect.ownKeys(client)
+  },
+  getOwnPropertyDescriptor(_target, prop) {
+    const client = prismaInstance ?? (prismaInstance = getClient())
+    return Reflect.getOwnPropertyDescriptor(client, prop)
+  },
+}) as PrismaClient
