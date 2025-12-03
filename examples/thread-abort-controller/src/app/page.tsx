01460befcc391f3e5e9fdfc5c65f6e7be3997e29
Domas Bitvinskas <domas.bitvinskas@me.com>
2025-12-04 00:16:24 +0200

    Add abort controller example

---
+++ examples/thread-abort-controller/src/app/page.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import {
+  SuperinterfaceProvider,
+  Thread,
+  AssistantProvider,
+  useMessages,
+  useSuperinterfaceContext,
+} from '@superinterface/react'
+import { Theme, Flex } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 10000,
+    },
+  },
+})
+
+const NestedComponent = () => {
+  const { messages } = useMessages()
+  const superinterfaceContext = useSuperinterfaceContext()
+
+  console.log('NestedComponent', {
+    messages: messages.length,
+    current: superinterfaceContext.createMessageAbortControllerRef.current,
+    refObject: superinterfaceContext.createMessageAbortControllerRef,
+  })
+
+  if (!superinterfaceContext.createMessageAbortControllerRef.current) {
+    return <button disabled>No abort controller</button>
+  }
+
+  return (
+    <button
+      onClick={() => {
+        superinterfaceContext.createMessageAbortControllerRef.current?.abort()
+      }}
+    >
+      Abort
+    </button>
+  )
+}
+
+export default function Page() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Theme
+        accentColor="lime"
+        grayColor="gray"
+        appearance="light"
+        radius="medium"
+        scaling="100%"
+      >
+        <SuperinterfaceProvider
+          variables={{
+            publicApiKey: 'e6709062-c641-44d0-a9e5-9e1da1099907',
+            assistantId: '816b577b-bd8d-4d1c-8ff2-93a45055c242',
+          }}
+        >
+          <AssistantProvider>
+            <Flex
+              flexGrow="1"
+              height="100dvh"
+              p="5"
+            >
+              <Thread.Root>
+                <NestedComponent />
+                <Thread.Messages />
+                <Thread.MessageForm />
+              </Thread.Root>
+            </Flex>
+          </AssistantProvider>
+        </SuperinterfaceProvider>
+      </Theme>
+    </QueryClientProvider>
+  )
+}
