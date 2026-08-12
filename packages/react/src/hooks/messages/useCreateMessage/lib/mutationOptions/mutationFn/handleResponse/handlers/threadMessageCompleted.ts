9d93e18e2389170c1a3b017f0d09ca68e9afbf5e
Domas Bitvinskas <domas.bitvinskas@me.com>
2026-08-12 20:29:43 +0300

    Remove completion handler churn

--- packages/react/src/hooks/messages/useCreateMessage/lib/mutationOptions/mutationFn/handleResponse/handlers/threadMessageCompleted.ts
+++ packages/react/src/hooks/messages/useCreateMessage/lib/mutationOptions/mutationFn/handleResponse/handlers/threadMessageCompleted.ts
@@ -1,7 +1,9 @@
 import { replace } from 'radash'
 import { ThreadMessageCompletedEvent } from '@/types'
 import { extendMessage } from './extendMessage'
-import { QueryClient } from '@tanstack/react-query'
+import {
+  QueryClient,
+} from '@tanstack/react-query'
 import { MessagesQueryKey } from '@/types'
 
 export const threadMessageCompleted = ({
@@ -12,35 +14,35 @@ export const threadMessageCompleted = ({
   messagesQueryKey: MessagesQueryKey
   value: ThreadMessageCompletedEvent
   queryClient: QueryClient
-}) =>
-  queryClient.setQueryData(messagesQueryKey, (prevData: any) => {
-    if (!prevData) {
+}) => (
+  queryClient.setQueryData(
+    messagesQueryKey,
+    (prevData: any) => {
+      if (!prevData) {
+        return {
+          pageParams: [],
+          pages: [
+            {
+              data: [],
+              hasNextPage: false,
+              lastId: null,
+            },
+          ],
+        }
+      }
+
+      const [latestPage, ...pagesRest] = prevData.pages
+
       return {
-        pageParams: [],
+        ...prevData,
         pages: [
           {
-            data: [],
-            hasNextPage: false,
-            lastId: null,
+            ...latestPage,
+            data: replace(latestPage.data, extendMessage({ message: value.data, messages: latestPage.data }), (m) => m.id === value.data.id),
           },
+          ...pagesRest,
         ],
       }
     }
-
-    const [latestPage, ...pagesRest] = prevData.pages
-
-    return {
-      ...prevData,
-      pages: [
-        {
-          ...latestPage,
-          data: replace(
-            latestPage.data,
-            extendMessage({ message: value.data, messages: latestPage.data }),
-            (m) => m.id === value.data.id,
-          ),
-        },
-        ...pagesRest,
-      ],
-    }
-  })
+  )
+)
